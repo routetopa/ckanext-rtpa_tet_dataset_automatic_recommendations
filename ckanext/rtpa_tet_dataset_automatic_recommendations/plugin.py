4777641477c2a8c067e7412cbebfbc7a5b162293
Arek Stasiewicz <arekstasiewicz@mail.com>
2018-01-05 18:08:03 +0000

    part2

--- ckanext/rtpa_tet_dataset_automatic_recommendations/plugin.py
+++ ckanext/rtpa_tet_dataset_automatic_recommendations/plugin.py
@@ -7,7 +7,7 @@ from ckan.lib.base import BaseController
 from ckan.common import json, response, request
 import ckan.model as model
 import os
-from ckan.lib.base import c#, g, h
+from ckan.lib.base import c, g, h
 
 
 def get_recommended_datasets(pkg_id):
