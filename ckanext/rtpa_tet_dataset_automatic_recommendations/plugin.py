f4ec5553ee37107ad616bc7a0edc0c92e4c559ba
Maciej Janowski <maciej@janowski.co>
2018-01-05 13:35:15 +0000

    Changes from my local machine

--- ckanext/rtpa_tet_dataset_automatic_recommendations/plugin.py
+++ ckanext/rtpa_tet_dataset_automatic_recommendations/plugin.py
@@ -10,19 +10,15 @@ import os
 from ckan.lib.base import c#, g, h
 
 
-class Rtpa_Tet_Dataset_Automatic_RecommendationsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-
-
-    def get_recommended_datasets(pkg_id):
+def get_recommended_datasets(pkg_id):
     package = toolkit.get_action('package_show')(None, {'id': pkg_id.strip()})
     response_data  = {}
     if "linked_datasets" in package and package["linked_datasets"] != "":
         l = []
         pkgs = package["linked_datasets"].split(",")
         for pkg in pkgs:
-            log.debug("PKG_ID:"+pkg_id)
-            log.debug("type of:"+str(type(pkg_id)))
+            #log.debug("PKG_ID:"+pkg_id)
+            #log.debug("type of:"+str(type(pkg_id)))
             p = toolkit.get_action('package_show')(None, {'id': pkg})
             item = {}
             item ["name"] = pkg
@@ -50,7 +46,7 @@ class Rtpa_Tet_Dataset_Automatic_RecommendationsPlugin(plugins.SingletonPlugin):
             'q': q,
             'rows': 5
         }
-        log.debug(q)
+        #log.debug(q)
         response_data["datasets"] = toolkit.get_action('package_search')(None, data_dict)["results"]
         for ds in response_data["datasets"]:
             if ds["name"] == pkg_id:
@@ -59,9 +55,27 @@ class Rtpa_Tet_Dataset_Automatic_RecommendationsPlugin(plugins.SingletonPlugin):
 
 
 
+class Rtpa_Tet_Dataset_Automatic_RecommendationsPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
+
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'rtpa_tet_dataset_automatic_recommendations')
+        
+    @staticmethod
+    def after_map(m):
+         m.connect('get_recommended_datasets', '/api/3/util/tet/get_recommended_datasets',
+           controller='ckanext.rtpa_tet_dataset_automatic_recommendations.plugin:RtpaApi', action='get_recommended_datasets')
+         return m
+        
+        
+class RtpaApi(BaseController):
+    def get_recommended_datasets(self):
+        response.content_type = 'application/json; charset=UTF-8'
+        pkg_id = request.params["pkg"]
+        data = get_recommended_datasets(pkg_id)
+        return json.dumps(data)
